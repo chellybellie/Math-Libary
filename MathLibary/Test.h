04c76a22f7f757ba8a0ccf6918a24e211ebbd809
chellybellie <chellybellie@gmail.com>
2016-09-26 11:42:22 -0700

    som function headers

--- MathLibary/Test.h
+++ MathLibary/Test.h
@@ -3,3 +3,16 @@
 int donothing(int x);
 
 float test_quad(float x);
+
+struct quad_results
+
+{
+	float left_root, right_root;
+	int roots;
+};
+quad_results quad(float a, float b, float c);
+
+float lerp(float start, float end, float t);
+
+struct point { float x, y; };
+float distance(point p1, point p2);
--- MathLibary/test.cpp
+++ MathLibary/test.cpp
@@ -8,3 +8,10 @@ float test_quad(float x)
 {
 	return x*x + 2 *x - 7;
 }
+
+quad_results quad(float a, float b, float c)
+{
+	
+	return  ;
+}
+
