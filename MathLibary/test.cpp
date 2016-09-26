b94b599282c2c3c8df6ce97178c644ac95ad913f
chellybellie <chellybellie@gmail.com>
2016-09-26 10:59:52 -0700

    mathLib

---
+++ MathLibary/test.cpp
@@ -0,0 +1,5 @@
+#include "Test.h"
+int donothing(int x)
+{
+	return x*x + 2*x + 5;
+}
---
+++ math test/main.cpp
@@ -0,0 +1,9 @@
+#include <cassert>
+#include <iostream>
+#include "Test.h"
+int main()
+{
+	printf("Hello World! %d", donothing(40));
+	getchar();
+
+}
